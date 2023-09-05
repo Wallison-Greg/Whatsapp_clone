87b7eabee11a0ee2eb0e22d45ea57afb847e3f34
wallison da silva gregorio <wallisongregorio2022@gmail.com>
2023-09-05 12:06:41 -0300

    header do app finalizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
 
+//icons
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
 function App() {
   return (
     <div className="App">
-      wpp_clone
+      <div className='sidebar'>
+        <header>
+
+          <img className='header--avatar' src='https://pps.whatsapp.net/v/t61.24694-24/361328749_2611165299191006_5229913777978113971_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQeLZ7dgTo06AVpNXXv0cfNVwxa_mznnusAMfbainY5ZA&oe=6504358C&_nc_cat=104'/>
+
+          <div className='header--buttons'>
+            <div className='header--btn'>
+              <DonutLargeIcon/>
+            </div>
+            <div className='header--btn'>
+              <ChatIcon/>
+            </div>
+            <div className='header--btn'>
+              <MoreVertIcon/>
+            </div>
+          </div>
+
+        </header>
+
+        <div className='search'>
+          <div className='search--input'>
+              <SearchIcon/>
+              <input type='search' placeholder='procurar ou começar uma nova conversa'/>
+          </div>
+        </div>
+        <div className='chatlist'>...</div>
+      </div>
+      <div className='contentarea'>...</div>
     </div>
   );
 }
